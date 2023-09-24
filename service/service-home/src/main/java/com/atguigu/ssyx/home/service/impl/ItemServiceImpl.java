3266b8d51dab94561b4ba762f39880dd04401bfd
Harrison <liufg0515@163.com>
2023-09-24 13:41:46 +0800

    second commit

---
+++ service/service-home/src/main/java/com/atguigu/ssyx/home/service/impl/ItemServiceImpl.java
@@ -0,0 +1,90 @@
+package com.atguigu.ssyx.home.service.impl;
+
+import com.atguigu.ssyx.activity.client.ActivityFeignClient;
+import com.atguigu.ssyx.client.product.ProductFeignClient;
+import com.atguigu.ssyx.client.search.SkuFeignClient;
+import com.atguigu.ssyx.home.service.ItemService;
+import com.atguigu.ssyx.vo.product.SkuInfoVo;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private ProductFeignClient productFeignClient;
+
+    @Autowired
+    private ActivityFeignClient activityFeignClient;
+
+    @Autowired
+    private SkuFeignClient skuFeignClient;
+    @Resource
+    private ThreadPoolExecutor threadPoolExecutor;
+    //详情
+    @Override
+    public Map<String, Object> item(Long skuId, Long userId) {
+        Map<String, Object> result = new HashMap<>();
+
+        //skuId查询
+        CompletableFuture<SkuInfoVo> skuInfocompletableFuture =
+                CompletableFuture.supplyAsync(() -> {
+            //远程调用获取sku对应数据
+            SkuInfoVo skuInfoVo = productFeignClient.getSkuInfoVo(skuId);
+            result.put("skuInfoVo", skuInfoVo);
+            return skuInfoVo;
+        },threadPoolExecutor);
+
+        //sku对应优惠卷信息
+        CompletableFuture<Void> activityCompletableFuture = CompletableFuture.runAsync(() -> {
+            //远程调用获取优惠卷
+            Map<String,Object> activityMap =
+                    activityFeignClient.findActivityAndCoupon(skuId,userId);
+            result.putAll(activityMap);
+        },threadPoolExecutor);
+
+        //更新商品热度
+        CompletableFuture<Void> hotCompletableFuture = CompletableFuture.runAsync(() -> {
+            //远程调用更新热度
+            skuFeignClient.incrHotScore(skuId);
+        },threadPoolExecutor);
+
+        //任务组合
+
+//
+//        CompletableFuture allCompletableFuture=CompletableFuture.allOf(
+//                skuInfocompletableFuture,
+//                activityCompletableFuture,
+//                hotCompletableFuture
+//        );
+
+        CompletableFuture.allOf(
+                skuInfocompletableFuture,
+                activityCompletableFuture,
+                hotCompletableFuture
+        ).join();
+//        allCompletableFuture.whenComplete((result, ex) -> {
+//            if (ex != null) {
+//                if (skuInfocompletableFuture.isCompletedExceptionally()) {
+//                    System.err.println("skuInfocompletableFuture failed: " + ex.getMessage());
+//                }
+//                if (activityCompletableFuture.isCompletedExceptionally()) {
+//                    System.err.println("activityCompletableFuture failed: " + ex.getMessage());
+//                }
+//                if (hotCompletableFuture.isCompletedExceptionally()) {
+//                    System.err.println("hotCompletableFuture failed: " + ex.getMessage());
+//                }
+//            }
+//        }).join();
+
+        return result;
+    }
+}
